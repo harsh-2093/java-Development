e175d263ae7cae668d3366d44da28686414647f4
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-03-28 22:09:09 +0530

    in this i learnt java syntax, operator ,psvm,loops,string handling,how variable /duplicated values are stored/

---
+++ hello.java
@@ -0,0 +1,137 @@
+class hello
+{
+    public static void main(String a[])
+    {
+      // int num1=9;
+      // long l1=234l;
+      // boolean h=true;
+      // int bin=1_00_00_00;
+      // int num2=0x7E;
+      // double num1=12e10;
+      // char num1='a';
+      // // num1+=1;
+      // int num1=(int)5.6;
+      // byte b=127;
+      // int c = 257;
+      // byte k=(byte)c;
+      // float f=5.6f;
+      // int t=(int)f;
+      // int num1=7;
+      // int num2=5;
+      // double t=num1%num2;
+      // num1=num1+2;
+      // num1+=3;
+      // num1++;
+      // num1--;
+      // System.out.println(num1);
+      // int result=num1++;
+      // System.out.println(result);
+      // System.out.println(num1);
+
+      // double x=6.00;
+      // int y=6;
+
+      // int c=2;
+      // int b=3;
+      // boolean result=x<=y && c!=b;
+      // System.out.println(result);
+    // int x=11;
+
+    // if(x>10)
+    // {
+    //   System.out.println("hi");
+    // }
+    // else
+    // {
+    //   System.out.println("bye");
+    // }
+
+    // int n=4;
+    // int result=0;
+    
+    // if(n%2==0)
+    // {
+    //   result=10;
+    // }
+    // else{
+    //   result=20;
+    // // }
+    // result=n%2==0?10:20;
+    // System.out.println(result);
+             
+
+    // int s=9;
+
+    // switch (s) {
+    //   case 1:
+    //     System.out.println("mon");
+    //     break;
+    //   case 2:
+    //     System.out.println("tue");
+    //     break;
+    //   case 3:
+    //     System.out.println("wed");
+    //     break;
+    //   case 4:
+    //     System.out.println("thu");
+    //     break;
+    
+    //   default:
+    //     System.out.println("other");
+    //     break;
+    // }
+    // int num=10;
+
+    // while(true)
+    // {
+    //   if(num==0)break;                        
+    //   System.out.println("hi");
+    //   num--;
+    // }
+    // int i=1;
+    // while(i<=4)
+    // {
+    //   System.out.println("hi"+i);
+
+    //   int j=0;
+
+    //   while(j<=3)
+    //   {                        
+    //     System.out.println("hello"+j);
+    //     j++;
+    //   }
+    //   i++;
+    // }
+
+    // int i=0;
+
+    // do{
+    //   System.out.println("here the loop start free line");
+    //   i++;
+    // }
+    // while(i<2);
+
+    // for( i=0;i<5;i++)
+    // {
+    //   System.out.println("ji ji ez");
+    // }
+
+
+    // for(int i=1;i<=7;i++)
+    // {
+    //   for(int j=1;j<=24;j++)
+    //   {
+    //     if(j>=1 && j<=12)
+    //     {
+    //     System.out.println("day"+i+"hour"+j+"am");                            
+    //     }
+    //     else{
+    //     System.out.println("day"+i+"hour"+j%12+"pm");
+
+    //     }
+    //   }
+    // }
+
+      
+    }
+}
