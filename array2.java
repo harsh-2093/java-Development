a782b9d0cc3295e6b8df36b2712750e7ee0d488c
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-03-31 21:40:30 +0530

    string handling

---
+++ array2.java
@@ -0,0 +1,9 @@
+public class array2 {
+  public static void main(String[] args) {
+    int arr[]=new int[4];
+    //no expansion fixed size
+    
+
+
+  }
+}
---
+++ string/basics.java
@@ -0,0 +1,18 @@
+package string;
+
+public class basics {
+	public static void main(String[] args) {
+		String name=new String("harsh tripathi");
+		System.out.println("hello "+name);
+//		System.out.println(name.hashCode());
+		System.out.println(name.charAt(0));
+		name=name.concat("raja ji");
+		System.out.println(name);
+		
+		//immutable string
+		
+		//string buffer
+		
+		
+	}
+}
