3145109152f6d5f07bbf2ff1e9506e23d82413d2
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-04-04 11:01:16 +0530

    understand about upcasting and down casting of classes

---
+++ casting.java
@@ -0,0 +1,23 @@
+class a
+{
+	public void show1()
+	{
+		System.out.println("in a show");
+	}
+}
+class b extends a
+{
+	public void show2()
+	{
+		System.out.println("in b show");
+	}
+}
+public class casting {
+	public static void main(String[] args) {
+//		a obj=(a)new b();                      //this is called upcasting this is implicit
+//		obj.show1();
+		a obj=(a)new b();
+		obj.show2();
+		
+	}
+}
