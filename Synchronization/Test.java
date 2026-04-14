e2f98b71ba684a36d5e01c14fa12ac7b1693d627
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-04-14 20:40:39 +0530

    learn about critical condition race and then mutual exclusion (synchronization )to prevent race condition

---
+++ Synchronization/Test.java
@@ -0,0 +1,26 @@
+package Synchronization;
+
+public class Test {
+	public static void main(String[] args) {
+		Counter cnt=new Counter();
+		MyThread t1=new MyThread(cnt);
+		MyThread t2=new MyThread(cnt);
+		t1.start();
+		t2.start();
+		try
+		{
+			t1.join();
+			t2.join();
+			
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+		System.out.println(cnt.getCount());
+		
+		
+	}
+}
+ 
