12a0a23c1026a7cb5e28e760aaa72e9214963929
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-04-25 22:59:17 +0530

    deadlock in thread and how to avoid it by ordering

--- optionalClass/Demo.java
+++ optionalClass/Demo.java
@@ -2,18 +2,19 @@ package optionalClass;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
+// import java.util.Optional;
+// import java.util.stream.Stream;
 
 public class Demo {
   public static void main(String[] args) {
     List<String> names=Arrays.asList("nmi","harsh","john","yami");
 
-    Optional<String> name=names.stream()
+    String name=names.stream()
                       .filter(str -> str.contains("x"))
-                      .findFirst();
+                      .findFirst()
+                      .orElse("not found");
 
-    System.out.println(name.orElse("name not found"));
+    System.out.println(name);
 
 
   }
